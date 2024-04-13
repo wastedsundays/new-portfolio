6a2a1aefbed6cd753053bbec53f02866536474c4
Adam H <ad.hauck@gmail.com>
2024-04-12 23:29:46 -0700

    added single work page rest path and loading

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,16 +1,56 @@
 import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from 'react';
 
 
 function SinglePage() {
+    const { slug } = useParams()
+
+    const restPath = `https://adamh.ca/portfolio/wordpress/wp-json/wp/v2/fwd-projects?acf_format=standard&slug=${ slug }&_embed`
+    const [restData, setData] = useState([])    
+    const [isLoaded, setLoadStatus] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(restPath)
+            if ( response.ok ) {
+                const data = await response.json()
+                setData(data)
+                setLoadStatus(true)
+            } else {
+                setLoadStatus(false)
+            }
+        }
+        fetchData()
+    }, [restPath])
+
+
+
     return (
         <div className=''>
             <p>Hello From the Single Page</p>
             <p>This page will display individual projects with details etc.</p>
-            <Link to='../contact'>
-                <button>
-                    Contact
-                </button>
-            </Link>
+            { isLoaded ?
+                <div>
+                    <h2>{restData[0].title.rendered}</h2>
+                    
+                    <Link to='../contact'>
+                        <button>
+                            Contact
+                        </button>
+                    </Link>
+                    <Link to='../work'>
+                        <button>
+                            Back
+                        </button>
+                    </Link>
+
+                </div>
+                : 
+                <div>
+                    Loading...
+                </div>
+            }
 
         </div>
     )
