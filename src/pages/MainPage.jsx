0a9f78fab6f9d4dd6e6085f1f7f8815252993191
Adam H <ad.hauck@gmail.com>
2024-04-16 01:28:31 -0700

    minor formatting

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,10 +5,16 @@ import { Link } from "react-router-dom"
 function MainPage() {
     return (
         <div className=''>
-            <p>Hello From the Home Page</p>
+            <section>
+
+                <p>Hello From the Home Page</p>
+
+            </section>
             
             <section>
-            <p>What I do Section - work</p>
+
+                <p>What I do Section - work</p>
+                <p>Here we will have either one project featured image, or a carousel showing a few featured projects</p>
 
                 <Link to='work'>
                     <button>
@@ -19,6 +25,7 @@ function MainPage() {
 
             <section>
                 <p>Who I am section - about</p>
+                <p>This will not have an image. Just a teaser text to get them to click on the link</p>
                 <Link to='about'>
                     <button>
                         About Me
@@ -28,6 +35,7 @@ function MainPage() {
 
             <section>
                 <p>Contact section - get in touch</p>
+                <p>Say something nice to direct them to the contact page. Or, have the form be here.</p>
                 <Link to='contact'>
                     <button>
                         Contact
