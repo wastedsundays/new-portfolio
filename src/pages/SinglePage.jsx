0aa5d450ab1028448ba5a4ce79db92553869da85
Adam H <ad.hauck@gmail.com>
2024-05-10 18:55:06 -0700

    got tools images and content from post on single page

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -9,12 +9,12 @@ function SinglePage() {
     const restPath = `${REST_PATH}ahdesigns-work?acf_format=standard&slug=${ slug }&_embed`
     const [restData, setData] = useState([])    
     const [isLoaded, setLoadStatus] = useState(false)
-
+    const [toolsData, setToolsData] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(restPath)
-            if ( response.ok ) {
+            if (response.ok) {
                 const data = await response.json()
                 setData(data)
                 setLoadStatus(true)
@@ -22,35 +22,51 @@ function SinglePage() {
                 setLoadStatus(false)
             }
         }
+
         fetchData()
     }, [restPath])
 
+    useEffect(() => {
+        const fetchToolsData = async () => {
+            const response = await fetch('https://wastedsundays.com/adamhdesign/wp-json/wp/v2/ahdesigns-tools?acf_format=standard&per_page=100')
+            if (response.ok) {
+                const data = await response.json()
+                setToolsData(data)
+            }
+        }
 
+        fetchToolsData()
+    }, [])
 
-    
+    const getToolImage = (toolTitle) => {
+        const tool = toolsData.find(tool => tool.title.rendered === toolTitle)
+        return tool ? tool.acf.tool_image : null
+    }
 
     return (
         <div className=''>
-            { isLoaded ?
+            {isLoaded ? (
                 <div>
-                    <h2>{restData[0].title.rendered}</h2>
                     
-                    {restData[0].acf.work_tools && restData[0].acf.work_tools.length > 0 &&
+                    <h2>{restData[0].title.rendered}</h2>
+                    <div dangerouslySetInnerHTML={{__html:restData[0].content.rendered }}></div>
+
+                    {restData[0].acf.work_tools && restData[0].acf.work_tools.length > 0 && (
                         <div>
                             <h3>Tools Used</h3>
-                            {restData[0].acf.work_tools.map((tool, i) => 
+                            {restData[0].acf.work_tools.map((tool, i) => (
                                 <div key={i}>
                                     <div className={tool.post_title}>
-                                        {tool.ID}
-                                        {tool.post_title}
-        
+                                        {/* {tool.ID}
+                                        {tool.post_title} */}
+                                        
+                                        <img src={getToolImage(tool.post_title)} alt={`${tool.post_title} icon card`} />
                                     </div>
                                 </div>
-                            )}
+                            ))}
                         </div>
-                    }
+                    )}
 
-                    
                     <Link to='../contact'>
                         <button>
                             Contact
@@ -63,11 +79,11 @@ function SinglePage() {
                     </Link>
 
                 </div>
-                : 
+            ) : (
                 <div>
                     Loading...
                 </div>
-            }
+            )}
 
         </div>
     )
